d7c6beeafe16741816c9095b9465175082969060
喻永权 <rainy.yu@wwwarehouse.com>
2018-02-28 19:15:36 +0800

    Initial Commit

---
+++ RainOpencv/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RainOpencv
+//
+//  Created by 喻永权 on 2018/2/28.
+//  Copyright © 2018年 喻永权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
